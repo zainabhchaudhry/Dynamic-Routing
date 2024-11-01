bc8c558af627415a31f5448f934ba713b22591b3
Zainab Hassan Chaudhry <141686134+zainabhchaudhry@users.noreply.github.com>
2024-11-02 03:14:46 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import Link from 'next/link'
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-   <div>
-    <h1>Dynamic Routing</h1>
-   </div>
-  );
+    <div>
+        <h1>House of Coders</h1>
+        <ul>
+            <li><Link href="Home/About">About</Link></li>
+            <li><Link href= "Home/Services">Services</Link></li>
+            <li><Link href= "Home/Careers">Careers</Link></li>
+            <li><Link href= "Home/Contact">Contact</Link></li>
+        </ul>
+    </div>
+  )
 }
+export default page
